e318fa80a2768c76c1fa4562b1d7c7b5f7da92f5
Hafiz Naufal <hafiznaufal27lp@gmail.com>
2026-05-30 00:32:25 +0700

    fix cors for production deploy

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 app.use(
